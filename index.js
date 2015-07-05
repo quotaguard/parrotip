20d70af7c4882ce8b02209ab2d7fef39ab1f7d28
Tim Williams <tim@teachmatic.com>
2015-07-05 11:07:59 +0100

    add plain output

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.json({ ip: request.ip })
+app.get('/', function(req, res) {
+    res.send({ ip: req.ip });
+});
+
+app.get('/plain', function(req, res) {
+    res.send(req.ip);
 });
 
 app.listen(app.get('port'), function() {
